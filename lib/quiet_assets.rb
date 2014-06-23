d1ff3b960b9f3114b2aa72094383f611b29c4534
vala <gonjo@free.fr>
2014-06-23 23:09:33 +0200

    add a parameter to allow not logging assets errors and to configure more assets prefixes

--- lib/quiet_assets.rb
+++ lib/quiet_assets.rb
@@ -4,12 +4,19 @@ module QuietAssets
   class Engine < ::Rails::Engine
     # Set as true but user can override it
     config.quiet_assets = true
+    config.quiet_assets_prefix = nil
+    config.quiet_assets_level = Logger::ERROR
 
     initializer 'quiet_assets', :after => 'sprockets.environment' do |app|
       next unless app.config.quiet_assets
       # Parse PATH_INFO by assets prefix
-      ASSETS_PREFIX = %r[\A/{0,2}#{app.config.assets.prefix}]
+      ASSETS_PREFIX = if app.config.quiet_assets_prefix
+        %r[\A/{0,2}(#{app.config.assets.prefix}|#{app.config.quiet_assets_prefix})]
+      else
+        %r[\A/{0,2}#{app.config.assets.prefix}]
+      end
       KEY = 'quiet_assets.old_level'
+      LOG_LEVEL = app.config.quiet_assets_level
       app.config.assets.logger = false
 
       # Just create an alias for call in middleware
@@ -18,7 +25,7 @@ module QuietAssets
           begin
             if env['PATH_INFO'] =~ ASSETS_PREFIX
               env[KEY] = Rails.logger.level
-              Rails.logger.level = Logger::ERROR
+              Rails.logger.level = LOG_LEVEL
             end
             call_without_quiet_assets(env)
           ensure
